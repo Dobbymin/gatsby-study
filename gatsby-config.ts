3a207902c59826f6a6c7e808229224832e957df7
Dobbymin <gmin990624@gmail.com>
2024-11-29 20:31:16 +0900

    feat: chakra ui 설정 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-typescript",
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
         allExtensions: true,
@@ -32,5 +32,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if `false`, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {number} [portalZIndex=40]
+         * The z-index to apply to all portal nodes. This is useful
+         * if your app uses a lot z-index to position elements.
+         */
+        portalZIndex: 40,
+      },
+    },
   ],
-}
+};
